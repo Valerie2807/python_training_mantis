93e7976ee55da81b41dd0944ebb44b6243e935ae
valeriyavishnevskaya <v.vishnevskaja@medpoint24.ru>
2020-12-27 12:16:05 +0300

    Задание 25.Добавлена проверка списков проектов и проверка предусловий

--- test/test_add_project_mantis.py
+++ test/test_add_project_mantis.py
@@ -1,6 +1,9 @@
 from model.project import Project
 
 
-def test_add_project(app, json_project):
+def test_add_project(app, db, json_project):
     app.session.login("administrator", "root")
+    old_project = db.get_project_list()
     app.project.create_new_project(json_project)
+    new_project = db.get_project_list()
+    assert len(old_project) == len(new_project)
--- test/test_delete_project_mantis.py
+++ test/test_delete_project_mantis.py
@@ -1,8 +1,11 @@
 import random
 
 
-def test_delete_project(app, db):
+def test_delete_project(app, db, json_project):
     app.session.login("administrator", "root")
+    app.project.open_project_page()
+    if len(db.get_project_list()) == 0:
+        app.project.create_new_project(json_project)
     app.project.open_project_page()
     old_project = db.get_project_list()
     project = random.choice(old_project)
